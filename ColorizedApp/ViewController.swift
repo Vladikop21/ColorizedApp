71d5788283a8c94590cef4f6047207514a6d228f
Vladdoriy <vladik.karpitskiy@gmail.com>
2024-05-31 09:17:46 +0300

    set up sliders

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorizedView: UIView!
+    
+    @IBOutlet weak var redLabelValue: UILabel!
+    @IBOutlet weak var blueLabelValue: UILabel!
+    @IBOutlet weak var greenLabelValue: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        redLabelValue.text = redSlider.value.formatted()
+        colorizedView.backgroundColor = colorizedView.backgroundColor
+        
+    }
+    
+    @IBAction func greenSliderAction() {
+        
+        
+    }
+    
+    @IBAction func blueSliderAction() {
+        
+        
+    }
+    private func redSlidersetup() {
+        redSlider.value = 0
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+        
+    }
+    
+    
 }
+
+
 
